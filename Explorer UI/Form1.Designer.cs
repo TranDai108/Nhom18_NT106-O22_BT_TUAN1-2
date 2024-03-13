fe0350ee7ff2019ebed1712b19bee090a5205344
Lê Ngọc Kiều Anh <162035553+lengockieuanh@users.noreply.github.com>
2024-03-13 15:12:52 +0700

    file Explorer UI

---
+++ Explorer UI/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Explorer_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBack = new System.Windows.Forms.Button();
+            this.btForward = new System.Windows.Forms.Button();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btBack
+            // 
+            this.btBack.Location = new System.Drawing.Point(13, 19);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(56, 34);
+            this.btBack.TabIndex = 0;
+            this.btBack.Text = "<<";
+            this.btBack.UseVisualStyleBackColor = true;
+            this.btBack.Click += new System.EventHandler(this.btBack_Click);
+            // 
+            // btForward
+            // 
+            this.btForward.Location = new System.Drawing.Point(75, 19);
+            this.btForward.Name = "btForward";
+            this.btForward.Size = new System.Drawing.Size(56, 34);
+            this.btForward.TabIndex = 0;
+            this.btForward.Text = ">>";
+            this.btForward.UseVisualStyleBackColor = true;
+            this.btForward.Click += new System.EventHandler(this.btForward_Click);
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(726, 19);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(63, 34);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(137, 23);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(581, 26);
+            this.tbPath.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(18, 65);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(771, 364);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "vo duyen nua";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "hi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 465);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.btForward);
+            this.Controls.Add(this.btBack);
+            this.Name = "Form1";
+            this.Text = "Explorer UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBack;
+        private System.Windows.Forms.Button btForward;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Explorer UI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser1.Url = new Uri(fbd.SelectedPath);
+                    tbPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btBack_Click(object sender, EventArgs e)
+        {
+            if(webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void btForward_Click(object sender, EventArgs e)
+        {
+            if(webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
